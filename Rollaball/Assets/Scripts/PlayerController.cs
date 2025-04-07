c87b5fd0b8531cb49db2160b36990f604d68ccac
Rivera-Angel <rivera.angel2808@gmail.com>
2025-04-07 16:58:13 -0700

    Fixed .gitignore

---
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+    private int jumpCount = 0;
+
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public float jumpForce = 5f;
+    public int maxJumps = 2;
+    public GameObject winTextObject;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 7)
+        {
+            winTextObject.SetActive(true);
+        }    
+    }
+
+    void OnJump()
+    {
+        if (jumpCount < maxJumps)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpCount++;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 0;
+        }
+    }
+
+}
